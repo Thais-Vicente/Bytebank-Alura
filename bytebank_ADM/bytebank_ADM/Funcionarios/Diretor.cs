8e207408b609b0870541d503e2ec70e07f013238
Thais-Vicente <vicente.thais@yahoo.com.br>
2022-09-08 00:18:02 -0300

    Arquivos adicionados

---
+++ bytebank_ADM/bytebank_ADM/Funcionarios/Diretor.cs
@@ -0,0 +1,32 @@
+﻿using bytebank_ADM.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Diretor:Funcionario
+    {
+       
+        public Diretor(string cpf):base(cpf, 5000)
+        {
+            Console.WriteLine("Criando um diretor.");
+        }
+
+        public override void AumentarSalario()
+        {
+            this.Salario *= 1.15;
+        }
+
+        public override double GetBonificacao()
+        {
+
+            return Salario * 0.5;
+
+        }
+
+
+    }
+}
---
+++ bytebank_ADM/bytebank_ADM/Utilitários/GerenciadorDeBonificacao.cs
@@ -0,0 +1,33 @@
+﻿using bytebank_ADM.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Utilitários
+{
+    public class GerenciadorDeBonificacao
+    {
+        private double totalBonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            this.totalBonificacao += funcionario.GetBonificacao();
+        }
+            
+        public double getBonificacao()
+        {
+            return this.totalBonificacao;
+        }
+
+        internal string GetBonificacao()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
+
+
